f239204e58efdfb7d7f5332d1e53146f981faa53
GintasS <svedgintas@gmail.com>
2021-04-08 23:53:40 +0300

    Added main class, a cell, that will hold all the data for the cell.

---
+++ src/SpreadsheetEvaluator.Domain/Models/MathModels/Cell.cs
@@ -0,0 +1,66 @@
+﻿using SpreadsheetEvaluator.Domain.Extensions;
+using SpreadsheetEvaluator.Domain.MathModels;
+using SpreadsheetEvaluator.Domain.Models.Enums;
+
+namespace SpreadsheetEvaluator.Domain.Models.MathModels
+{
+    public class Cell
+    {
+        public string Key { get; set; }
+        public CellValue Value { get; set; }
+    }
+
+    public class CellValue
+    {
+        public object Value { get; private set; }
+        public CellType CellType { get; private set; } = CellType.NotSet;
+        public bool IsFormulaCell => Value is Formula;
+
+        public bool IsErrorCell => CellType == CellType.Error;
+
+        public CellValue(object value)
+        {
+            TryUpdateCell((dynamic)value);
+        }
+
+        public void TryUpdateCell(object value)
+        {
+            var cellType = value.TryGetCellTypeFromValue();
+
+            if (cellType == null)
+            {
+                return;
+            }
+
+            if (value.IsNumber())
+            {
+                decimal.TryParse(value.ToString(), out decimal decimalValue);
+                Value = decimalValue;
+            }
+            else if (value is string)
+            {
+                Value = new string(value.ToString());
+            }
+            else if (value is bool)
+            {
+                Value = (bool)value;
+            }
+            else if (value is Formula formula)
+            {
+                Value = new Formula(formula);
+            }
+            else if (value is CellValue cellValue)
+            {
+                Value = cellValue.GetValue();
+            }
+
+            CellType = cellType.Value;
+        }
+
+        public void SetCellAsErrorCell()
+        {
+            CellType = CellType.Error;
+            Value = "type does not match";
+        }
+    }
+}
