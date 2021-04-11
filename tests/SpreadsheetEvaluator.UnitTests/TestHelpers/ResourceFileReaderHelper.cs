7136f1e7006d6eab7a3899d32c0fb92b1d40f0ff
GintasS <svedgintas@gmail.com>
2021-04-11 13:37:50 +0300

    Moved ResourceFileReader to a separate folder.

---
+++ tests/SpreadsheetEvaluator.UnitTests/TestHelpers/ResourceFileReaderHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace SpreadsheetEvaluator.UnitTests.TestHelpers
+{
+    public static class ResourceFileReaderHelper
+    {
+        public static string ReadFile(string path)
+        {
+            using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+            using var streamReader = new StreamReader(stream);
+            return streamReader.ReadToEnd();
+        }
+    }
+}
